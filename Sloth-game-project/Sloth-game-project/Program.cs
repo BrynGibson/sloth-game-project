4c4cedb6c09a1eaaf00badcd9223f5d8a85bddeb
BrynGibson <50122617+BrynGibson@users.noreply.github.com>
2019-05-31 12:27:59 +1200

    vs project created

---
+++ Sloth-game-project/Sloth-game-project/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sloth_game_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
