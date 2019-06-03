0ff9e2a05b72a6ba39c8ee4035acfe7d48418c56
Matthew Gillan (1000013698) <GILLMD3@student.op.ac.nz>
2019-06-04 10:39:55 +1200

    yeetbix

--- Sloth-game-project/Sloth-game-project/Program.cs
+++ Sloth-game-project/Sloth-game-project/Program.cs
@@ -8,6 +8,29 @@ namespace Sloth_game_project
 {
     class Program
     {
+        static void LevelOne()
+        {
+            Console.WriteLine("You are an exotic sloth, escaping dangerous poachers that want to sell you as a pet.");
+            Console.WriteLine("You make a narrow escape in the sewers, but you reach a few rooms that are seemingly a dead end.");
+            Console.WriteLine("You must escape the sewers and the poachers into the forest so you can have a life.");
+            Console.WriteLine("Press enter to continue..");
+            Console.ReadLine();
+            //Console.Clear();
+
+            Console.WriteLine("");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
         static void Main(string[] args)
         {
         }
